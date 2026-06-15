6805435ba29bb6b92cab9ac948d63f4ff0e8b7c2
Antipathico <riquelmemourasantos@gmail.com>
2026-06-15 17:22:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,29 +4,22 @@ function abrirScripts() {
     document.getElementById("scriptsPage").style.display = "block";
     window.scrollTo(0, 0);
 }
-
-
 function voltarHome() {
     document.getElementById("scriptsPage").style.display = "none";
     document.getElementById("homePage").style.display = "block";
     window.scrollTo(0, 0);
 }
-
+console.log("Criador @gustin_cdt & antipathico")
 function copiar(id) {
-
     let texto =
         document.getElementById(id).innerText;
-
     navigator.clipboard.writeText(texto);
-
     const btn = event.target;
-
 btn.innerText = "✅ Copiado!";
-
 setTimeout(() => {
     btn.innerText = "Copiar";
 }, 2000);
 }
 
 
-console.log("Criador @gustin_cdt & antipathico")
+
